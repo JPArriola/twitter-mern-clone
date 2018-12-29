35060fcf5e4876defe8adb79d27cc5155585fda4
Joshua Arriola <joshuapatrickarriola@gmail.com>
2018-12-28 19:48:34 -0800

    finish backend

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const express = require("express");
 const app = express();
 const db = require('./config/keys').mongoURI;
 const port = process.env.PORT || 5000;
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const bodyParser = require('body-parser');
+const passport = require('passport');
 
 mongoose
 .connect(db, { useNewUrlParser: true })
@@ -11,3 +15,12 @@ mongoose
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 app.get("/", (req, res) => res.send("Hello Josh"));
+
+app.use(passport.initialize());
+require('./config/passport')(passport);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
