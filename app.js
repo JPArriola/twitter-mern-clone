5f8b9ed5cb9263399872c86356eda987a1e9fb3c
Joshua Arriola <joshuapatrickarriola@gmail.com>
2018-12-29 17:43:55 -0800

    Fix error with application undefined token

--- app.js
+++ app.js
@@ -2,18 +2,17 @@ const mongoose = require('mongoose');
 const express = require("express");
 const app = express();
 const db = require('./config/keys').mongoURI;
-const port = process.env.PORT || 5000;
-const users = require("./routes/api/users");
-const tweets = require("./routes/api/tweets");
 const bodyParser = require('body-parser');
 const passport = require('passport');
+
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
 
 mongoose
 .connect(db, { useNewUrlParser: true })
 .then(() => console.log("Connected to MongoDB successfully"))
 .catch(err => console.log(err));
 
-app.listen(port, () => console.log(`Server is running on port ${port}`));
 app.get("/", (req, res) => res.send("Hello Josh"));
 
 app.use(passport.initialize());
@@ -24,3 +23,6 @@ app.use(bodyParser.json());
 
 app.use("/api/users", users);
 app.use("/api/tweets", tweets);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server is running on port ${port}`));
--- routes/api/users.js
+++ routes/api/users.js
@@ -56,32 +56,37 @@ router.post('/register', (req, res) => {
 });
 
 router.post('/login', (req, res) => {
-  const { errors, isValid } = validateLoginInput(req.body);
+    const { errors, isValid } = validateLoginInput(req.body);
 
-  if (!isValid) {
-    return res.status(400).json(errors);
-  }
+    console.log(errors);
 
-  const email = req.body.email;
-  const password = req.body.password;
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
 
-  User.findOne({email})
-    .then(user => {
-      if (!user) {
-        // Use the validations to send the error
-        errors.email = 'User not found';
-        return res.status(404).json(errors);
-      }
-
-      bcrypt.compare(password, user.password)
+    const email = req.body.email;
+    const password = req.body.password;
+  
+    User.findOne({email})
+      .then(user => {
+        if (!user) {
+          return res.status(404).json({email: 'This user does not exist'});
+        }
+  
+        bcrypt.compare(password, user.password)
         .then(isMatch => {
-          if (isMatch) {
-            res.json({msg: 'Success'});
-          } else {
-            // And here:
-            errors.password = 'Incorrect password'
-            return res.status(400).json(errors);
-          }
+            if (isMatch) {
+            const payload = {id: user.id, name: user.name};
+
+              jwt.sign(payload, keys.secretOrKey, { expiresIn: 3600 }, (err, token) => {
+                res.json({
+                  success: true,
+                  token: "Bearer " + token
+                });
+              });
+            } else {
+                return res.status(400).json({password: 'Incorrect password'});
+            }
         })
     })
 })
